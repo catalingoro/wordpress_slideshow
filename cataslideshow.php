74197e78f4d1decdbc1fdd47c3f5aa9fbaa7a5e1
CatalinG <gorovei_catalin@yahoo.com>
2017-05-01 18:23:12 +0200

    Plugin Name
    Slideshow Plugin Details

---
+++ cataslideshow.php
@@ -0,0 +1,15 @@
+<?php 
+/*
+ * Plugin Name: Cata Slideshow
+ * Plugin URI: 
+ * Description: This plugin displays an Slideshow gallery.
+ * Version: 0.1
+ * Author: Catalin G
+ * Author URI: http://
+ * License: GPL v2+
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: Cata Slideshow
+ * Domain Path: 
+ */
+
+ ?>
