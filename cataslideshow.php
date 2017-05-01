a53cf9d1cb118c260a3562b4078592bebdb8d5bc
CatalinG <gorovei_catalin@yahoo.com>
2017-05-01 19:03:47 +0200

    function registers and includes 3 important javascript files
    function registers and includes 3 important javascript files

--- cataslideshow.php
+++ cataslideshow.php
@@ -27,7 +27,27 @@ add_action('init', 'cs_init');
 add_action('wp_print_scripts', 'cs_register_scripts');
 add_action('wp_print_styles', 'cs_register_styles');
 
-
+function cs_register_scripts() {
+    if (!is_admin()) {
+        // register
+        wp_register_script('cs_cata-script', plugins_url('standard/js/jquery.slides.min.js', __FILE__), array( 'jquery' ));
+        wp_register_script('cs_script', plugins_url('standard/js/script.js', __FILE__));
+ 
+        // enqueue
+        wp_enqueue_script('cs_cata-script');
+        wp_enqueue_script('cs_script');
+    }
+}
+ 
+function cs_register_styles() {
+    // register
+    wp_register_style('cs_styles', plugins_url('standard/css/font-awesome.min.css', __FILE__));
+    wp_register_style('cs_styles_theme', plugins_url('standard/css/example.css', __FILE__));
+ 
+    // enqueue
+    wp_enqueue_style('cs_styles');
+    wp_enqueue_style('cs_styles_theme');
+}
 
 
 ?>
