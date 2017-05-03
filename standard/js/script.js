ac114dde3893550765f8c0eeed399660fd858496
CatalinG <gorovei_catalin@yahoo.com>
2017-05-03 13:15:23 +0200

    Replace JS code
    Replace JS code

--- standard/js/script.js
+++ standard/js/script.js
@@ -1,3 +1,16 @@
 jQuery(document).ready(function($) {
-	$('#slideshow').cataSlidershow();
+	$('#slides_shortcode').slidesjs({
+	    width: 940,
+	    height: 529,
+	    navigation: false
+	  });
+
+	$('#slides_widget').slidesjs({
+	    width: 180,
+	    height: 100,
+	    navigation: false
+	  });
 });
+
+
+  
